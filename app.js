0c10ebc7dec75f671b1dc13bac05e99209f77b7d
gabrielspinoza <Espinoza.Gabriel@outlook.com>
2022-02-04 18:14:59 -0500

    added routes for get and post api request

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
+
+var routes = require('./routes/routes');
 // var usersRouter = require('./routes/users');
 // var actorRouter = require('./routes/actor');
 
@@ -37,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', routes);
 // app.use('/users', usersRouter);
 // app.use('/actor', actorRouter);
 
